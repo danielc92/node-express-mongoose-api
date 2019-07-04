95570c1a9e37e5817feac48abe80c9ad41f74a42
Daniel Corcoran <danielc.92@outlook.com>
2019-07-04 11:26:06 +1000

    Added listener

--- server.js
+++ server.js
@@ -62,3 +62,7 @@ app.delete('/cities', jsonParser, (request, response) => {
     .then(result => response.json({result}))
     .catch(error => response.json({error}))
 })
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`))
