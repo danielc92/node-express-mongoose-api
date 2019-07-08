965f949228714070ccad9009d6da92dc8065a383
Daniel Corcoran <danielc.92@outlook.com>
2019-07-08 15:07:17 +1000

    Added login function

--- routes/users.route.js
+++ routes/users.route.js
@@ -6,9 +6,28 @@ const User = require('../models/Users');
 
 
 router.post('/login', (request,response) => {
-    response.json({message: "login"})
+    const {email, password} = request.body
 
-})
+    User.findOne({email})
+        .then(user => {
+            if (user) {
+                return bcrypt.compare(password, user.password)
+                      .then(result => {
+                          if (result) {
+                              const token = user.makeToken();
+                              response.json(token);
+                          } else {
+                              response.status(400).json({message: "Invalid credentials. Try Again."})
+                          }
+                      })
+                      
+            } else {
+                response.status(400).json({message: "Invalid credentials. Try Again."})
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+});
 
 router.post('/register', (request, response) => {
     response.json({message: "register"})
