165a140043b7818428bac17586dadcfd687b616e
Daniel Corcoran <danielc.92@outlook.com>
2019-07-08 16:29:53 +1000

    Fixed logger not logging

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3002;
-//
-// Third party logger
+
+// [NOTE] Log with morgan package
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a'})
+app.use(morgan('combined', { stream: accessLogStream }))
 
 
 // Database settings
 const mongoose = require('mongoose');
 const { db_port, db_name, db_driver, db_host} = require('./settings/config');
 const uri = `${db_driver}://${db_host}:${db_port}/${db_name}`
+
 
 // Route settings
 const cityRoutes = require('./routes/cities.route');
@@ -26,11 +29,6 @@ app.use('/api/auth', userRoutes);
 mongoose.connect(uri, { useNewUrlParser: true })
         .then(()=>console.log('Server connection with mongodb successful'))
         .catch(error => console.log(`An error has occured ${error}`))
-
-
-// [NOTE] Log with morgan package
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a'})
-app.use(morgan('combined', { stream: accessLogStream }))
 
 
 // [NOTE] Listen on designated PORT for incoming requests
