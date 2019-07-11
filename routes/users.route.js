8d187816abe2e9c8ba42c0f15f10f9651adfd5c7
Daniel Corcoran <danielc.92@outlook.com>
2019-07-11 13:45:32 +1000

    Fixed registration error,   , users being saved after being invalidated

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,8 +1,8 @@
 const settings = require('../settings/config');
 const bcrypt = require('bcrypt');
 const express = require('express');
-const router = express.Router();
 const User = require('../models/Users');
+const router = express.Router();
 
 
 router.post('/login', (request,response) => {
@@ -27,10 +27,19 @@ router.post('/login', (request,response) => {
 });
 
 
+/*
+Registeration route has multiple steps
+- Check if the email exists, if not return 400 status code, else continue
+- Create new user object with request.body and validate using inbuilt mongoose method
+- Return 400 status code if the validation failed, else continue
+- Hash the password using bcrypt
+- Save the user object to MongoDB
+- Send a response to the user upon successful account creation
+*/
 router.post('/register', async(request, response) => {
 
     let user = await User.findOne({email: request.body.email})
-    console.log('user: ', user)
+
     if (user) return response.status(400).json({message: 'This email is taken.'})
 
     user = new User({
@@ -39,18 +48,26 @@ router.post('/register', async(request, response) => {
         password: request.body.password
     })
 
-    user.validate(function(error) {
-        if (error) {
-            return response.status(400).json({message: error.message})
-        }
+    let validation_errors = await user.validate()
+                                      .then(result => result)
+                                      .catch(error => error)
+
+    if (validation_errors) return response.status(400).json({
+        message: "Validation has failed", 
+        validation_errors
     })
 
     user.password = await bcrypt.hash(user.password, settings.bcrypt_iterations)
     await user.save();
 
     const token = user.makeToken();
+
     response.header(token).json({
-        message: "Account has been created."
+        message: "Account has been successfully created.",
+        user: { 
+            email: user.email, 
+            createdAt: user.createdAt
+        }
     })
 })
 
